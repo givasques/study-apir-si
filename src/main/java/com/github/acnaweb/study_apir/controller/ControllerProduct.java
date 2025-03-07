c086e8daf15ad818e72601e7802a191590813495
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-06 22:53:18 -0300

    feat: add springframework features

--- src/main/java/com/github/acnaweb/study_apir/controller/ControllerProduct.java
+++ src/main/java/com/github/acnaweb/study_apir/controller/ControllerProduct.java
@@ -4,24 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.acnaweb.study_apir.model.Product;
 import com.github.acnaweb.study_apir.service.ProductService;
 
 @RestController
 @RequestMapping("produtos")
 public class ControllerProduct {
 
-    @Autowired
-    private ProductService productService = new ProductService();
+    @Autowired //avisa o spring quais atributos precisa de ingestao automatica
+    private ProductService productService;
 
     @PostMapping
-    public ResponseEntity<String> create() {
-        productService.createProduct(null);
-        return ResponseEntity.status(201).body("Produto cadastrado");
+    public ResponseEntity<Product> create(@RequestBody Product product) {
+        Product productCreated = productService.createProduct(product);
+        return ResponseEntity.status(201).body(productCreated);
     }
 
     @DeleteMapping
@@ -31,15 +34,15 @@ public class ControllerProduct {
 
     }
 
-    @PutMapping
-    public ResponseEntity<String> update() {
-        productService.updateProduct(null, null);
-        return ResponseEntity.status(200).body("Produto atualizado");
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product) {
+        Product productUpdated = productService.updateProduct(id, product);
+        return ResponseEntity.status(200).body(productUpdated);
     }
 
     @GetMapping
     public ResponseEntity<String> find() {
         productService.getProductById(null);
-        return ResponseEntity.status(200).body("Maça");
+        return ResponseEntity.status(200).body("Pêra");
     }
 }
--- src/main/java/com/github/acnaweb/study_apir/service/ProductService.java
+++ src/main/java/com/github/acnaweb/study_apir/service/ProductService.java
@@ -1,26 +1,39 @@
 package com.github.acnaweb.study_apir.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.github.acnaweb.study_apir.model.Product;
 
-@Service
+@Service //spring automaticamente instancia essa classe em memória
 public class ProductService {
+    
+    private List<Product> products = new ArrayList<>();
 
-    private List<Product> products;
+    // para controlar o id dos produtos cadastrados
+    private long sequence = 1L;
 
     public Product createProduct(Product product) {
-        return null;
+        product.setId(sequence++);
+        products.add(product);
+        return product;
     }
 
     public Product getProductById(Long id) {
         return null;
     }
 
-    public Product updateProduct(Long id, Product product) {
-        return null;
+    public Optional<Product> updateProduct(Long id, Product product) {
+        return products.stream()
+                .filter(e -> e.getId().equals(id))
+                        .findFirst()
+                        .map(p -> {
+                            p.setNome(product.getNome()); 
+                            return p;
+                        });
     }
 
     public void deleteProduct(Long id) {
