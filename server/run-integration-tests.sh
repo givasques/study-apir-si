8d08f9ae5e03c900b22560144162c982aeddd44d
Giovanna Vasques <133826730+givasques@users.noreply.github.com>
2025-10-31 01:44:38 +0000

    feat: update script shell - add get all and put

---
+++ server/run-integration-tests.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e
+
+apt update
+apt install -y jq
+
+#CRIAR UM PRODUTO
+HTTP_STATUS=$(
+    curl -X 'POST' \
+    'http://localhost:9000/api/v3/produtos' \
+    -H 'accept: */*' \
+    -H 'Content-Type: application/json' \
+    -w "%{http_code}" \
+    -o product_created.json \
+    -d '{
+    "nome": "Melancia"
+}')
+
+echo "Status do produto criado: $HTTP_STATUS"
+
+if [ "$HTTP_STATUS" -ne 201 ]; then
+    echo "Erro ao criar produto"
+    exit 1
+fi
+
+PRODUTO_ID=$(jq '.id' product_created.json)
+
+echo "Produto criado com ID: $PRODUTO_ID"
+
+#PEGAR PRODUTOS
+HTTP_STATUS=$(curl -X 'GET' \
+  'http://localhost:9000/api/v3/produtos' \
+  -o product_list.json \
+  -w "%{http_code}" \
+  -H 'accept: */*')
+
+echo "Status HTTP: $HTTP_STATUS"
+
+if [ "$HTTP_STATUS" -ne 200 ]; then
+    echo "Erro ao acessar a API de produtos"
+    exit 1
+fi
+
